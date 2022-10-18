13a1bea6c8c7757e30dae376a37d4f8a160c496d
Zilchmaster <zilchmaster@users.noreply.github.com>
2022-10-18 14:14:42 -0400

    Removed unnecessary comments

--- server/server.js
+++ server/server.js
@@ -7,9 +7,6 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
-//var MongoStore = require('connect-mongo');
-
-//mongodb+srv://Zilchmasta:nsy%23nfJn6qszayP8@cluster0.vgzfh4x.mongodb.net/registrationFormHeruko?retryWrites=true&w=majority
 
 mongoose.connect('mongodb+srv://Zilchmasta:nsy%23nfJn6qszayP8@cluster0.vgzfh4x.mongodb.net/registrationForm?retryWrites=true&w=majority', {
   useNewUrlParser: true,
