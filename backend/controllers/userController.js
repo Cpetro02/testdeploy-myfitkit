56ebf4527e0fb4d7915f350d1d23e8072cacbeb1
allysonvasquez <allysonvasquez01@gmail.com>
2022-11-11 11:14:58 -0500

    signup api working

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,9 +4,16 @@ const loginUser = async(req, res) => {
     res.json({mssg: 'login user page'})
 }
 
-//signup user
+//signup user - creates acc in MongoDB
 const signupUser = async(req, res) => {
-    res.json({mssg: 'sign up user page'})
+    const {email, username, password} = req.body
+
+    try{
+        const user = await User.signup(email, username, password) //call function to signup and create user account in MongoDB
+        res.status(200).json({email, user}) //send response of successful acc creation
+    } catch(error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 module.exports = {loginUser, signupUser}
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,7 @@
 //a model of a user that uses our app
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+const bcrypt = require('bcrypt')
 
 const connection = mongoose.createConnection("mongodb+srv://admin:admin@fitkittens.znqjo0j.mongodb.net/dev_test_user");
 
@@ -21,10 +22,25 @@ userSchema = new Schema( {
 		type: String,
 		required: true,
 	},
-	passwordConf: {
-		type: String,
-		required: true,
-	}
 }, {collection: 'users'})
+
+//create static method to sign a user up
+userSchema.statics.signup = async function(email, username, password) {
+
+	//check if email already exists (if it does, then says account already exists)
+	const exists = await this.findOne({ email })
+	if(exists){
+		throw Error('Email/Account already exists.')
+	}
+
+	//else, hash the password using bcrypt & create an account
+	const salt = await bcrypt.genSalt(10) //creates random string to add on to password
+	const hash = await bcrypt.hash(password, salt)
+
+	const user = await this.create({ email, username, password:hash }) // create + send the user account with hashed password to MongoDB
+
+	return user
+}
+
 User = connection.model('User', userSchema);
 module.exports = User;
